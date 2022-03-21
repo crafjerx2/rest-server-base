6571de74f37a9b88e320d042b036ea1c6d59252c
Carlos Pineda <carlos.pineda.made@gmail.com>
2022-03-21 01:24:58 -0400

    add methods get, post, put, delete and patch to route

--- models/Server.js
+++ models/Server.js
@@ -15,9 +15,35 @@ class Server {
     }
 
     routes() {
-        this.app.get('/', (req, res) => {
-            res.send('Hello World!')
-        })
+        this.app.get('/api', (req, res) => {
+            res.json({
+                'msg': 'get API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                'msg': 'post API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                'msg': 'put API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                'msg': 'delete API'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                'msg': 'patch API'
+            });
+        });
     }
 
     listen() {
