19a3eabb490e71861578d45ec3030e24c2febb39
Carlos Pineda <carlos.pineda.made@gmail.com>
2022-03-22 15:50:06 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const {Schema, mongoose} = require('mongoose');
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo electrónico es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    },
+});
+
+modoule.exports = model('User', userSchema);
